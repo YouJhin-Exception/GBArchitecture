472531ea6e487714752f91acbab13ffd4f2265f6
«YouJhin» <palevich86@mail.ru>
2023-07-13 17:57:28 +0300

    hw 2 prt - 2 - :blue_car: :rowboat: :construction_worker: :gem:

---
+++ Patterns/src/observer/Main.java
@@ -0,0 +1,22 @@
+package observer;
+
+import observer.Users.OfficialUser;
+import observer.Users.RecruiterUser;
+import observer.Users.RegularUser;
+import observer.classes.Page;
+
+public class Main {
+    public static void main(String[] args) {
+        Page page = new Page();
+        page.addNewPost("1-rst post");
+
+        RegularUser user = new RegularUser(page);
+        page.addNewPost("Have a n1s day");
+
+        RecruiterUser recruiterUser = new RecruiterUser(page);
+        OfficialUser officialUser = new OfficialUser(page);
+
+        page.addNewPost("Latest post for all");
+    }
+
+}
