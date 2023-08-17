6ffb182740ec936f94f7f6bb5e11eafd72d74549
«YouJhin» <palevich86@mail.ru>
2023-08-17 16:29:39 +0300

    hw - 11 done :see_no_evil:

---
+++ RobotCleaner/autService/logic/Token.java
@@ -0,0 +1,27 @@
+package autService.logic;
+
+import java.util.Date;
+
+public class Token {
+    private String id;
+    private String value;
+    private Date expirationDate;
+
+    public Token(String id, String value, Date expirationDate) {
+        this.id = id;
+        this.value = value;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+}
---
+++ RobotCleaner/autService/logic/intefaces/IAccessRepo.java
@@ -0,0 +1,8 @@
+package autService.logic.intefaces;
+
+import autService.logic.Token;
+
+public interface IAccessRepo {
+    void saveToken(Token token);
+    Token getToken(String tokenId);
+}
